b12465f25fca6286dd4769fb176b3de7e20c9e8e
Lucas Moraes <lucashsmoraes@gmail.com>
2018-11-23 02:41:46 -0200

    docker-compose modificado e conectando ao Mongo

--- src/main/java/com/spring/mongo/resources/UserResource.java
+++ src/main/java/com/spring/mongo/resources/UserResource.java
@@ -1,26 +1,25 @@
 package com.spring.mongo.resources;
 
+import com.spring.mongo.services.UserService;
 import com.spring.mongo.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User(null, "Maria Brown", "maria@gmail.com");
-        User alex = new User(null, "alex Green", "alex@gmail.com");
-
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
-        return ResponseEntity.ok().body(list);
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
     }
 }
---
+++ src/main/java/com/spring/mongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.spring.mongo.services;
+
+import com.spring.mongo.domain.User;
+import com.spring.mongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
